9c87287944f847740339f6fa9f1c778dd9544175
Edd-war <digiespirit.1807@gmail.com>
2021-11-20 21:12:56 -0600

    37. Implementando Semantic UI

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import { Button, Icon } from 'semantic-ui-react'
+
+
 export default function Home() {
     return ( 
         <div>
             <h1>Estamos en Next Js</h1>
+
+            <Button primary>Primary</Button>
+            <Button secondary>Secondary</Button>
+            <Icon name='home' />
         </div>
     );
 }
