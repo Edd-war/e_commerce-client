2265586b879d3ad3b2fc6de40e78d0f3d94712ee
Edd-war <digiespirit.1807@gmail.com>
2021-11-21 06:19:15 -0600

    48. Terminando Layout

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,37 @@ export default function Home() {
     return ( 
         <BasicLayout>
             <h1>Estamos en la HOME</h1>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
+            <h2>Hola Mundo</h2>
         </BasicLayout>
     );
 }
