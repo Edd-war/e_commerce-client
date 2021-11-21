2b70cc0468caeb17e23722c2812c899a1d6bec6b
Edd-war <digiespirit.1807@gmail.com>
2021-11-21 02:31:27 -0600

    42. TopBar - Buscador

--- components/Header/TopBar/TopBar.js
+++ components/Header/TopBar/TopBar.js
@@ -10,7 +10,7 @@ export default function TopBar() {
                         <Logo />
                     </Grid.Column>
                     <Grid.Column width={8} className="top-bar__right">
-                        <h2>Buscador</h2>
+                        <Search />
                     </Grid.Column>
                 </Grid>
             </Container>
@@ -25,5 +25,15 @@ function Logo(){
                 <Image src="/logo.png" alt="Gaming" />
             </a>
         </Link>
+    );
+}
+
+
+function Search() {
+    return (
+        <Input
+            id="search-game"
+            icon={{name:"search"}}
+        />
     );
 }
