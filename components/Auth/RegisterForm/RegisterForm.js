bea8bc25ab16e5d9244eac0510352fb29027fd6c
Edd-war <digiespirit.1807@gmail.com>
2021-11-23 20:15:57 -0600

    51. Formulario de registro de usuarios

--- components/Auth/RegisterForm/RegisterForm.js
+++ components/Auth/RegisterForm/RegisterForm.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Form, Button } from 'semantic-ui-react';
 
 export default function RegisterForm(props) {
     const { showLoginForm } = props;
     return (
-        <div>
-            <h1>RegisterForm</h1>
-            <button onClick={showLoginForm}>Login</button>
-        </div>
+        <Form className="login-form">
+            <Form.Input 
+              fluid icon='user' 
+              iconPosition='left' 
+              placeholder='Usuario' 
+              type="text"
+              name="username"
+            />
+            
+            <Form.Input 
+              fluid icon='user' 
+              iconPosition='left' 
+              placeholder='Nombre' 
+              type="text"
+              name="name"
+            />
+            
+            <Form.Input 
+              fluid icon='user' 
+              iconPosition='left' 
+              placeholder='Apellidos' 
+              type="text"
+              name="lastname"
+            />
+            
+            <Form.Input 
+              fluid icon='email' 
+              iconPosition='left' 
+              placeholder='Correo electrónico' 
+              type="email"
+              name="email"
+            />
+            
+            <Form.Input 
+              fluid icon='password' 
+              iconPosition='left' 
+              placeholder='Contraseña' 
+              type="password"
+              name="password"
+            />
+
+            <div className="actions">
+                <Button type="button" basic>
+                    Iniciar Sesión
+                </Button>
+                <Button type="submit" className="submit">
+                    Registrar
+                </Button> 
+            </div>
+        </Form>
     )
 }
