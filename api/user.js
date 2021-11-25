b4a425634b332660cfe7dc305aeb7fca54ae965f
Edd-war <digiespirit.1807@gmail.com>
2021-11-25 03:12:11 -0600

    76. Manejando los datos del formulario de contraseña

--- api/user.js
+++ api/user.js
@@ -111,4 +111,22 @@ export async function updateEmailApi(idUser, formData, logout) {
         console.log(error);
         return null;
     }
-}   //
+}
+
+export async function updatePasswordApi(idUser, formData, logout) {
+    try {
+        const url = `${BASE_PATH}/users/${idUser}`;
+        const params = {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        };
+        const result = await authFetch(url, params, logout);
+        return result ? result : null;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
--- components/Account/ChangePasswordForm/ChangePasswordForm.js
+++ components/Account/ChangePasswordForm/ChangePasswordForm.js
@@ -1,29 +1,65 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'semantic-ui-react'; 
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 
 export default function ChangePasswordForm(props) {
     const {user, logout} = props;
 
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: validationSchema(),
+        onSubmit: async (formData) => {
+            console.log(formData);
+        }
+    });
+
+
     return (
         <div className="change-password-form">
             <h4>Actualizar Contraseña</h4>
-            <Form>
+            <Form onSubmit={formik.handleSubmit}>
                 <Form.Group widths='equal'>
                     <Form.Input fluid
                         name="password"
                         label='Nueva Contraseña'
                         placeholder='Nueva Contraseña'
                         type='password'
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        error={formik.errors.password}
                     />
                     <Form.Input fluid
                         name="repeatPassword"
                         label='Confirmar Nueva Contraseña'
                         placeholder='Confirmar Nueva Contraseña'
                         type='password'
+                        onChange={formik.handleChange}
+                        value={formik.values.repeatPassword}
+                        error={formik.errors.repeatPassword}
                     />
                 </Form.Group>
                 <Button type='submit' className="submit">Actualizar Contraseña</Button>
             </Form>
         </div>
     );
+}
+
+function initialValues() {
+    return {
+        password: '',
+        repeatPassword: ''
+    }
+}
+
+function validationSchema() {
+    return Yup.object().shape({
+        password: Yup.string()
+            .min(6, 'La contraseña debe tener al menos 6 caracteres')
+            .required('La contraseña es obligatoria'),
+        repeatPassword: Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Las contraseñas no coinciden')
+            .required('La confirmación de la contraseña es obligatoria')
+    });
 }
