a0ff03885220eb5fe8c4171dc3a05acbd301cef9
Edd-war <digiespirit.1807@gmail.com>
2021-11-25 00:35:12 -0600

    69. Manejando los datos del formulario de cambio de nombre y apellidos

--- components/Account/ChangeNameForm/ChangeNameForm.js
+++ components/Account/ChangeNameForm/ChangeNameForm.js
@@ -1,28 +1,57 @@
 import React from 'react';
 import { Form, Button } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 
 export default function ChangeNameForm(props) {
     const { user } = props;
+    const formik = useFormik({
+        initialValues: initialValues(user.name, user.lastname),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: (formData) => {
+            console.log(formData);
+        }
+    });
+
     return (
         <div className="change-name-form">
             <h4>Cambia nombre y apellidos</h4>
-            <Form>
+            <Form onSubmit={formik.handleSubmit}>
                 <Form.Group widths="equal">
                     <Form.Input 
                         fluid label="Nombre"  
                         name="name" 
-                        placeholder="Nombre nuevo" 
-                        value={user.name}
+                        placeholder="Nombre nuevo"
+                        onChange={formik.handleChange} 
+                        value={formik.values.name}
+                        error={formik.errors.name && true}
                     />
                     <Form.Input 
                         fluid label="Apellidos" 
                         name="lastname" 
                         placeholder="Apellidos nuevos" 
-                        value={user.lastname}
+                        onChange={formik.handleChange}
+                        value={formik.values.lastname}
+                        error={formik.errors.lastname && true}
                     />
                 </Form.Group>
-                <Button className="submit">Guardar Cambios</Button>
+                <Button className="submit" type="submit">Guardar Cambios</Button>
             </Form>
         </div>
     );
+}
+
+function initialValues(name, lastname) { 
+    return {
+        name: name||'',
+        lastname: lastname||''
+    };
+}
+
+function validationSchema() { 
+    return {
+        name: Yup.string().required('El nombre es requerido'),
+        lastname: Yup.string().required('Los apellidos son requeridos')
+    };
 }
