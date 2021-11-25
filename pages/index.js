d0734019626937b0f30c182e077256a4d6a7133e
Edd-war <digiespirit.1807@gmail.com>
2021-11-24 19:45:24 -0600

    66. Creando la pagina del usuario

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import BasicLayout from "../layouts/BasicLayout";
 
 export default function Home() {
     return ( 
-        <BasicLayout>
+        <BasicLayout className="home">
             <h1>Estamos en la HOME</h1>
             <h2>Hola Mundo</h2>
             <h2>Hola Mundo</h2>
