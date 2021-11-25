331dc79947bb0c6c645c99a858ec27b95e044fd5
Edd-war <digiespirit.1807@gmail.com>
2021-11-24 23:44:52 -0600

    67. Bloqueando páginas y añadiendo estilos

--- pages/account.js
+++ pages/account.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import BasicLayout from '../layouts/BasicLayout';
+import { getMeApi } from '../api/user';
+import useAuth from '../hooks/useAuth';
 
 export default function Account() {
+    const [user, setUser] = useState(undefined);
+    const {auth, logout} = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        (async() => {
+            const response = await getMeApi(logout);
+            setUser(response||{});
+        })();
+    }, [auth, logout]);
+
+    if(user === undefined) return null;
+    if(!auth){
+        router.replace("/");
+        return null;
+    }
+    // console.log(user);
+    // console.log(auth);
     return (
         <BasicLayout className="account">
-            <h1>Account</h1>
+            <Configuracion />
         </BasicLayout>
-    )
+    );
 }
+
+function Configuracion() {
+    return (
+        <BasicLayout className="account">
+            <div className="account__configuration">
+                <div className="title">Configuración</div>
+                <div className="data">Formularios de configuración.</div>
+            </div>
+        </BasicLayout>
+    );
+}
+
