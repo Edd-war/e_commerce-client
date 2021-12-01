f564bd0802e46ca4a34d24badb933cfca4ef89bf
Edd-war <digiespirit.1807@gmail.com>
2021-11-30 20:53:41 -0600

    94. Obteniendo los últimos juegos añadidos

---
+++ api/game.js
@@ -0,0 +1,17 @@
+import { result } from "lodash";
+import { BASE_PATH } from "../utils/constants";
+
+export async function getLastGamesApi(limit){
+    try{
+        const limitItems = `_limit=${limit}`;
+        const sortItems = `_sort=createdAt:DESC`;
+        const url = `${BASE_PATH}/games?${limitItems}&${sortItems}`;
+        const response = await fetch(url);
+        const result = await response.json();
+        return result;
+    }
+    catch(error){
+        console.log(error);
+        return [];
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,26 @@
+import React, { useState, useEffect } from "react";
 import BasicLayout from "../layouts/BasicLayout";
+import { getLastGamesApi } from "../api/game";
+import { size } from "lodash";
 
 export default function Home() {
+    const [games, setGames] = useState(null);
+    console.log(games);
+
+    useEffect(() => {
+        (async () => {
+            const response = await getLastGamesApi(50);
+            if(size(response) > 0) {
+                setGames(response);
+            } else {
+                setGames(null);
+            }
+        })();
+    }, []);
+
     return ( 
         <BasicLayout className="home">
             <h1>Estamos en la HOME</h1>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
-            <h2>Hola Mundo</h2>
             <h2>Hola Mundo</h2>
         </BasicLayout>
     );
