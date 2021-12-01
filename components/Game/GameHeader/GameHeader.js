7721a4d8d3f6b614eead3378ce8e4cbb4ddd5e0c
Edd-war <digiespirit.1807@gmail.com>
2021-12-01 05:35:03 -0600

    106. Añadiendo la información del juego

--- components/Game/GameHeader/GameHeader.js
+++ components/Game/GameHeader/GameHeader.js
@@ -12,6 +12,39 @@ export default function GameHeader(props) {
             <Grid.Column mobile={16} tablet={6} computer={5}>
                 <Image src={poster.url} alt={title} fluid />
             </Grid.Column>
+            <Grid.Column mobile={16} tablet={10} computer={11}>
+                <Info game={game} />
+            </Grid.Column>
         </Grid>
+    );
+}
+
+function Info (props) {
+    const { game } = props;
+    const { title, summary, price, discount } = game;
+    // console.log(title);
+
+    return (
+        <>
+            <div className="game-header__title">
+                <h1>{title}</h1>
+                <Icon name="heart outline" link />
+            </div>
+            <div className="game-header__delivery">Entrega de 24 a 48 horas</div>
+            <div 
+                className="game-header__summary" 
+                dangerouslySetInnerHTML={{__html: summary}}
+            />
+            <div className="game-header__buy">
+                <div className="game-header__buy-price">
+                    <p>Precio: ${price}</p>
+                    <div className="game-header__buy-price-actions">
+                        <p>Descuento: {discount}%</p>
+                        <p>Ahorras: ${(price * discount / 100)}</p>
+                    </div>
+                </div>
+                <Button className="game-header__buy-btn">Comprar por: ${price - (price * discount / 100)}</Button>
+            </div>
+        </>
     );
 }
