90d0c66ae7189132db3c4e3f433c38574d7d8443
Edd-war <digiespirit.1807@gmail.com>
2021-12-01 07:22:21 -0600

    110. Abriendo las imágenes al hacer click

--- components/Game/CarouselScreenshots/CarouselScreenshots.js
+++ components/Game/CarouselScreenshots/CarouselScreenshots.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Image } from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react';
+import { Image, Modal } from 'semantic-ui-react'
 import Slider from 'react-slick';
-import { map } from 'lodash';
+import { map, set } from 'lodash';
 
 const setting ={
     className: 'carousel-screenshots',
@@ -17,20 +17,32 @@ const setting ={
 
 export default function CarouselScreenshots(props) {
     const { title, screenshoots } = props;
+    const [showModal, setShowModal] = useState(false);
+    const [urlImage, setUrlImage] = useState(null);
     // console.log(screenshoots);
 
+    const openIamge = (url) => {
+        setUrlImage(url);
+        setShowModal(true);
+    }
+
     return (
-        <Slider {...setting}>
-            {
-                map(screenshoots, ({id, url, name}) => (
-                    <Image
-                        key={id}
-                        src={url}
-                        alt={name}
-                        onClick={() => { console.log('Abrir Iamgen') }}
-                    />
-                )
-            )}
-        </Slider>
+        <>
+            <Slider {...setting}>
+                {
+                    map(screenshoots, ({id, url, name}) => (
+                        <Image
+                            key={id}
+                            src={url}
+                            alt={name}
+                            onClick={() => openIamge(url)}
+                        />
+                    )
+                )}
+            </Slider>
+            <Modal open={showModal} onClose={() => setShowModal(false)} size="large" >
+                <Image src={urlImage} alt={title} />
+            </Modal>
+        </>
     );
 }
