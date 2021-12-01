787d19cdd29b23178ee8da05ce22f7dc33c819dc
Edd-war <digiespirit.1807@gmail.com>
2021-11-30 23:33:18 -0600

    98. Obteniendo juegos por plataformas

--- pages/games/[platform].js
+++ pages/games/[platform].js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import BasicLayout from '../../layouts/BasicLayout';
+import { getGamesPlatformApi } from '../../api/game';
+
+const limitPerPage = 10;
 
 export default function Platform() {
     const { query } = useRouter();
     // console.log(router);
+
+    const [games, setGames] = useState(null);
+
+    useEffect(() => {
+        (async() => {
+            const response = await getGamesPlatformApi(
+                query.platform, 
+                limitPerPage, 
+                0
+            );
+            // console.log(response);
+            setGames(response);
+        })();
+    }, [query]);
+
     return (
         <BasicLayout className="platform">
             <h1>Estás en la plataforma: {query.platform}</h1>
