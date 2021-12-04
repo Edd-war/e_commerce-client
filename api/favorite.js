6e76157ad18d9c64cf1e5b758bf009f4c09e2eb7
Edd-war <digiespirit.1807@gmail.com>
2021-12-04 03:23:27 -0600

    115. Eliminar juegos de favoritos

--- api/favorite.js
+++ api/favorite.js
@@ -54,3 +54,28 @@ export async function setFavoriteApi(idFav, idUser, idGame, logout) {
         return null;
     }
 }
+
+export async function deleteFavoriteApi(idUser, idGame, logout) {
+    try{
+        const dataFound = await isFavoriteApi(idUser, idGame, logout);
+        if(size(dataFound) > 0){
+            const url = `${BASE_PATH}/favorites/${dataFound[0]?._id}`;
+            const params = {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+            const responseDelete = await authFetch(url, params, logout);
+            const dataDelete = await responseDelete.json();
+            // console.log(await dataDelete);
+            return await dataDelete;
+        } else {
+            console.log("El juego no está en tu lista de favoritos");
+            return null;
+        }
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
