1f98af85472b6c43684042ac9be369cb34f12620
Edd-war <digiespirit.1807@gmail.com>
2021-12-04 05:23:42 -0600

    118. Renderizando la lista de juegos

--- pages/wishlist.js
+++ pages/wishlist.js
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { size } from 'lodash';
+import { Loader } from 'semantic-ui-react'
+import { size, forEach } from 'lodash';
 import BasicLayout from '../layouts/BasicLayout';
 import { getFavoritesApi } from '../api/favorite';
 import useAuth from '../hooks/useAuth';
+import GameList from '../components/GamesList';
 
 export default function WishList() {
     const [games, setGames] = useState(null);
     const { auth, logout } = useAuth();
 
+    console.log(games);
+
     useEffect(() => {
-        (async () => {
+        const fetchFavorites = async () => {
             const response=await getFavoritesApi(auth.idUser, logout);
-            // console.log(response);
-            setGames(response);
-         })();
+            console.log(response);
+            // setGames(response);
+            if(size(response)>0){
+                const gamesList=[];
+                forEach(response, (data)=>{
+                    // console.log(data);
+                    gamesList.push(data.game);
+                });
+                setGames(gamesList);
+                console.log(gamesList);
+            } else {
+                setGames([]);
+            }
+         };
+        fetchFavorites();
     }, []);
 
     return (
@@ -23,7 +39,15 @@ export default function WishList() {
                     <h2>Wishlist</h2>
                 </div>
                 <div className="wishlist__block-content">
-                    <p>Lista de Juegos</p>
+                    {!games && <Loader active>Cargando...</Loader>}
+                    {games && size(games) === 0 && (
+                        <div className="data__not-found">
+                            <h3>No hay juegos en tu wishlist</h3>
+                        </div>
+                    )}
+                    {games && size(games) > 0 && (
+                        <GameList games={games} />
+                    )}
                 </div>
             </div>
         </BasicLayout>
