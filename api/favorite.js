3488b88cb98b3d2d478a61d377de3089fea1437f
Edd-war <digiespirit.1807@gmail.com>
2021-12-04 03:13:51 -0600

    Bug reparado: Creaba registro de favoritos sin el usuario ni el juego
    Implementación propia: Crear una escala, de crear a actualizar.
    
    Luego de haberse creado el registro, agregué otra función para actualizar el mismo, gracias al response despues de haber creado el registro vacío de el nuevo favorito (solicitud POST), se obtiene el id de ese registro creado, porteriormente se pasa a una función Update (solicitud PUT) que agregará ahora si el id de usuario y el id del juego, correspondientes a la situación del id del nuevo favorito creado.

--- api/favorite.js
+++ api/favorite.js
@@ -24,12 +24,32 @@ export async function addFavoriteApi(idUser, idGame, logout) {
             const responseCreate = await authFetch(url, params, logout);
             const dataCreate = await responseCreate.json();
             console.log(await dataCreate);
-            return await dataCreate;
+            return await setFavoriteApi(dataCreate.id ,idUser, idGame, logout);
         } else {
             console.log("El juego ya está en tu lista de favoritos");
             return null;
         }
     } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+export async function setFavoriteApi(idFav, idUser, idGame, logout) {
+    try{
+        const url = `${BASE_PATH}/favorites/${idFav}`;
+        const params = {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({user: idUser, game: idGame}),
+        };
+        const responseUpdate = await authFetch(url, params, logout);
+        const dataUpdate = await responseUpdate.json();
+        console.log(await dataUpdate);
+        return await dataUpdate;
+    }  catch (error) {
         console.log(error);
         return null;
     }
