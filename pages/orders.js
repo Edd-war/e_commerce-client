28c51ffd2f1ad501f6607bfb20a7a0d0b699a431
Edd-war <digiespirit.1807@gmail.com>
2021-12-05 06:31:03 -0600

    145. Obteniendo todos los pedidos del usuario

--- pages/orders.js
+++ pages/orders.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Grid  } from 'semantic-ui-react';
 import BasicLayout from '../layouts/BasicLayout';
+import { getOrdersApi } from '../api/order';
+import useAuth from '../hooks/useAuth';
+import { result } from 'lodash';
 
 export default function Orders() {
+    const { auth, logout } = useAuth();
+
+    const [orders, setOrders] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const response = await getOrdersApi(auth.idUser, logout);
+            const result = await response.json();
+            // console.log(result);
+            setOrders(result || []);
+        })();
+    }, []);
+
     return (
         <BasicLayout className="orders">
             <div className="orders__block">
