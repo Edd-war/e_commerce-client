dd11304e1a7dada74459e58cf0f07b988f1b7289
Edd-war <digiespirit.1807@gmail.com>
2021-12-05 09:27:49 -0600

    148. Modal para ver mas información

--- components/Orders/Order/Order.js
+++ components/Orders/Order/Order.js
@@ -3,39 +3,71 @@ import { Image, Icon } from 'semantic-ui-react';
 import Link from 'next/link';
 import moment from 'moment';
 import "moment/locale/es";
-import BasicLayout from '../../../layouts/BasicLayout/BasicLayout';
+import BasicModal from '../../Modal/BasicModal';
 
 export default function Order(props) {
     const { order } = props;
     const { game, totalPayment, createdAt, addressShipping } = order;
     const { title, poster, url } = game;
-    console.log(title);
+    const [showModal, setShowModal] = useState(false);
+
+    // console.log(title);
     return (
         <>
             <div className="order">
-                    <div className="order__header">
-                        <Link href={`/${url}`}>
-                            <a>
-                                <Image src={poster.url} alt={title} />
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="order__info">
-                        <h3>{title}</h3>
-                        <p>$ {totalPayment} </p>
-                    </div>
-                    <div className="order__other">
-                        <p className="order__other-date">
-                            {moment(createdAt).format('LL')} - {moment(createdAt).format('LT')}
-                        </p>
-                        <Icon 
-                            name="eye" 
-                            circular 
-                            link
-                            onClick={() => console.log('ver info')}
-                        />
-                    </div>
+                <div className="order__header">
+                    <Link href={`/${url}`}>
+                        <a>
+                            <Image src={poster.url} alt={title} />
+                        </a>
+                    </Link>
+                </div>
+                <div className="order__info">
+                    <h3>{title}</h3>
+                    <p>$ {totalPayment} </p>
+                </div>
+                <div className="order__other">
+                    <p className="order__other-date">
+                        {moment(createdAt).format('LL')} - {moment(createdAt).format('LT')}
+                    </p>
+                    <Icon 
+                        name="eye" 
+                        circular 
+                        link
+                        onClick={() => setShowModal(true)}
+                    />
+                </div>
             </div>
+            <AddressModal 
+                showModal={showModal} 
+                setShowModal={setShowModal} 
+                addressShipping={addressShipping}    
+                title={title}
+            />
         </>
+    );
+}
+
+function AddressModal(props){
+    const { showModal, setShowModal, addressShipping, title } = props;
+
+    return (
+        <BasicModal
+            show={showModal}
+            setShow={setShowModal}
+            size="tiny"
+            title={title}
+        >
+            <h3>Dirección de envío</h3>
+            <div>
+                <p>{addressShipping.name}</p>
+                <p>{addressShipping.address}</p>
+                <p>
+                {addressShipping.state}, {addressShipping.city}{" "}
+                {addressShipping.postalCode}
+                </p>
+                <p>{addressShipping.phone}</p>
+            </div>
+        </BasicModal>
     );
 }
